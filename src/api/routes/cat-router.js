bd2693ac28deddde56a805c784873d735ecc6901
Riya Poonia <riya06@MacBook-Pro.local>
2026-09-19 00:02:51 +0300

    Add thumbnail middleware

--- src/api/routes/cat-router.js
+++ src/api/routes/cat-router.js
@@ -1,11 +1,20 @@
 const express = require('express');
+
 const upload = require('../../middlewares/upload');
+const createThumbnail = require('../../middlewares/thumbnail');
+
 const catController = require('../controllers/cat-controller');
+
 const router = express.Router();
 
 router.get('/', catController.getCats);
+
 router.get('/:id', catController.getCatById);
-router.post('/', upload.single('cat'), catController.addCat);
+
+router.post('/', upload.single('cat'), createThumbnail, catController.addCat);
+
 router.put('/:id', catController.updateCat);
+
 router.delete('/:id', catController.deleteCat);
+
 module.exports = router;
